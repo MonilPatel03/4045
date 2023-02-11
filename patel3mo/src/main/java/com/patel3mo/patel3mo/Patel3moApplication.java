ff968c3678aadfd7099a919bc94bd261bf82f79e
Monil Patel <105329178+MonilPatel03@users.noreply.github.com>
2023-02-12 03:14:28 +0530

    commit1
    commit

---
+++ patel3mo/src/main/java/com/patel3mo/patel3mo/Patel3moApplication.java
@@ -0,0 +1,13 @@
+package com.patel3mo.patel3mo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Patel3moApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Patel3moApplication.class, args);
+	}
+
+}
---
+++ patel3mo/src/test/java/com/patel3mo/patel3mo/Patel3moApplicationTests.java
@@ -0,0 +1,13 @@
+package com.patel3mo.patel3mo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Patel3moApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
